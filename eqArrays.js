69da5808a0791675016bb4bc0a18be35ac791915
Keila Kopvillem <kmkopvillem@gmail.com>
2022-02-09 21:38:17 -0500

    fixed bug for eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -8,18 +8,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let match = "";
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      match = true;
-    } else {
-      match = false;
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
-  }
-  return match;
+  } return true;
 };
 
-// write code for eqArrays function
-// takes two arrays and returns true or false based on perfect match
-// test with assertEqual
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", 2, 3], ["1", 2, 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
