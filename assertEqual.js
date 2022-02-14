6e2f54bf2715bed32d8de657e63a385e00471eec
Keila Kopvillem <kmkopvillem@gmail.com>
2022-02-14 16:50:35 -0500

    refactored head and assertEqual functions

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,9 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`👍🏻👍🏻👍🏻Assertion passed: ${actual} === ${expected}`);
+    console.log(`👍👍👍Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log(`👎🏻👎🏻👎🏻Assertion failed: ${actual} !== ${expected}`);
+    console.log(`👎👎👎Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(9, 0);
-assertEqual("Tony", "Soprano");
-assertEqual("stew", "stew");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍🏻👍🏻👍🏻Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎🏻👎🏻👎🏻Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array1) {
   return array1[0];
 };
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
-assertEqual(head([7, 9, 1, 5, 6, 7]), 7);
+module.exports = head;
