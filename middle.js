87813dfbe9453615bf3fd0d9fbf587f025df2aa9
Keila Kopvillem <kmkopvillem@gmail.com>
2022-02-14 19:00:15 -0500

    middle function refactored and test file made

--- middle.js
+++ middle.js
@@ -1,25 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let match = "";
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      match = true;
-    } else {
-      match = false;
-    }
-  }
-  return match;
-};
 
-const assertArraysEqual = function(arr1, arr2) {
-  let assertion = "";
-  let comparison = eqArrays(arr1, arr2);
-  if (comparison === true) {
-    assertion = true;
-  } else {
-    assertion = false;
-  }
-  console.log(assertion);
-};
 
 const middle = function(array) {
   let result = [];
@@ -34,3 +13,5 @@ const middle = function(array) {
   }
   return result;
 };
+
+module.exports = middle;
