7b2eab424675cb35b3f3158314e31d17edba90bf
Keila Kopvillem <kmkopvillem@gmail.com>
2022-02-14 19:15:41 -0500

    head function refactored and test file added

--- head.js
+++ head.js
@@ -3,4 +3,6 @@ const head = function(array1) {
   return array1[0];
 };
 
+console.log(head(['5']));
+
 module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,15 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head')
+const assert = require('chai').assert;
+const head = require('../head');
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
-assertEqual(head([7, 9, 1, 5, 6, 7]), 7);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns undefined for empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
+
